4a6be5d9b7d67ba4d7292e9eb1a1d756b355276b
Joon Lee <seouri@gmail.com>
2011-02-10 02:35:51 -0500

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gacatta::Application.config.secret_token = 'd7a20b7ca25bdca653ef56af334ae463cf3222ae376335d32a95468d6267a77959c64805d68277acaa7f8dea8e21ad96663e5264915daab35305107bac27114a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Gacatta::Application.config.session_store :cookie_store, :key => '_gacatta_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Gacatta::Application.config.session_store :active_record_store
